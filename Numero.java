1a2de3736606fa7071f98d049a8d4d8081c08380
Jorge <jorgemaryeb11@gmail.com>
2017-12-05 12:27:36 +0100

    Documentacion

--- Numero.java
+++ Numero.java
@@ -7,23 +7,23 @@ public class Numero {
 			
 		}
 		else {
-			System.out.printf("El número %d introducido no es váĺido%n",numero);
+			System.out.printf("El número %d introducido no es váĺido %n",numero);
 			return;
 			}
 		
 		if (numero % 2 ==0)
-			System.out.printf("El número %d es par%n",numero);
+			System.out.printf("El número %d es par %n",numero);
 		else
-			System.out.printf("El número %d es impar%n",numero);
+			System.out.printf("El número %d es impar %n",numero);
 		
 		
 		if (numero % 3 == 0 && numero % 5 == 0)
-			System.out.printf("El número %d es múltiplo de tres y cinco a la vez%n",numero);
+			System.out.printf("El número %d es múltiplo de tres y cinco a la vez %n",numero);
 		else
 			System.out.printf("El número %d no es mútliplp de tres y cinco a la vez",numero);
 		
 		String digitos = numero + "";
 		int digitosTotales = digitos.length();
-		System.out.printf("El numero tiene %d cifras%n",digitosTotales);
+		System.out.printf("El numero tiene %d cifras %n",digitosTotales);
 	}
 }
